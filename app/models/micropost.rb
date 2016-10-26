65c8ddb3cff7e5c033c9df830518e14fefd4230c
gongaimin <goldeagle_1995@163.com>
2016-10-26 14:51:48 +0800

    using RubyMine Git

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,8 @@
 class Micropost < ActiveRecord::Base
+
+  # 多对一关系
+  belongs_to :user
+
+  # 验证字段长度;
+  validates :content , length: {maximum:140,minimum:10}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 # 用户模型,继承ActiveRecord,
 class User < ActiveRecord::Base
+  #一对多关系
+  has_many :microposts
+
 end
