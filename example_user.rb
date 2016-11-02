52dce80dbbb4ed2c46873ba997aab2ce1233834b
gongaimin <goldeagle_1995@163.com>
2016-11-02 12:16:57 +0800

    a

--- example_user.rb
+++ example_user.rb
@@ -1,13 +1,5 @@
 class Example_user
 
-  attr_accessor:name,email;
-  def initialize(attributes={})
-    @name=attributes[:name];
-    @email=attributes[:email];
-  end
 
-  def formatted_email
-    "#{@name} < #{@email}"
-  end
 
 end
